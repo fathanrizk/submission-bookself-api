039066fe48ba665fe82ca559caee86995e8429d4
unknown <fathanrizki001@gmail.com>
2023-12-28 13:25:51 +0700

    Testing Server

---
+++ src/routes.js
@@ -0,0 +1,29 @@
+const routes = [
+  {
+    method: "POST",
+    path: "/books",
+    handler: addBookHandler,
+  },
+  {
+    method: "PUT",
+    path: "/books/{id}",
+    handler: editBookByIdHandler,
+  },
+  {
+    method: "GET",
+    path: "/books",
+    handler: getAllBooksHandler,
+  },
+  {
+    method: "GET",
+    path: "/books/{id}",
+    handler: getBookByIdHandler,
+  },
+  {
+    method: "DELETE",
+    path: "/books/{id}",
+    handler: deleteBookByIdHandler,
+  },
+];
+
+module.exports = routes;
